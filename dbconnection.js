a896fb9bf4afe7310acacf1b2abd635fac444753
Mohammed Bilal K A <ufsdev123@gmail.com>
2025-09-09 17:57:40 +0530

    updates

--- dbconnection.js
+++ dbconnection.js
@@ -1,12 +1,10 @@
-var mysql = require("mysql2");
-var connection = mysql.createPool({
+const mysql = require("mysql2");
+const connection = mysql.createPool({
 
      host:"DESKTOP-IK6ME8M",
     user: "Bilal",
     password: "root",
     database: "neo_tronice",
-  
-    
 });
 connection.on("connect", function() {
    console.log("Connected to the database");
--- models/quotation_master.js
+++ models/quotation_master.js
@@ -2,48 +2,52 @@
  var fs = require('fs');
  var quotation_master=
  { 
- Save_quotation_master:function(quotation_master_,callback)
- { 
+Save_quotation_master: function (quotation_master_, callback) { 
+  return db.query(
+    "CALL Save_quotation_master(" +
+      "@Quotation_Master_Id_ :=?, @Customer_Id_ :=?, @PaymentTerms_ :=?, @Payment_Term_Description_ :=?, " +
+      "@TotalAmount_ :=?, @Subsidy_Amount_ :=?, @NetTotal_ :=?, @Product_Name_ :=?, @Warranty_ :=?, " +
+      "@Terms_And_Conditions_ :=?, @Quotation_Status_Id_ :=?, @Quotation_Status_Name_ :=?, @Created_By_ :=?, " +
+      "@Description_ :=?, @items_ :=?, @bill_of_materials_ :=?, @production_chart_ :=?, " +
+      "@advance_percentage_ :=?, @onmaterialdelivery_percentage_ :=?, @onWork_completetion_percentage_ :=?, " +
+      "@System_Price_Excluding_KSEB_Paperwork_ :=?, @KSEB_Registration_Fees_KW_ :=?, " +
+      "@KSEB_Feasibility_Study_Fees_ :=?, @Additional_Structure_Work_ :=?, " +
+      "@TaxableAmount_ :=?, @TotalGSTAmount_ :=?, @TotalGSTPercent_ :=?, @TotalAdCESS_ :=?" +
+      ")",
+    [
+      quotation_master_.Quotation_Master_Id,
+      quotation_master_.Customer_Id,
+      quotation_master_.PaymentTerms,
+      quotation_master_.Payment_Term_Description,
+      quotation_master_.TotalAmount,
+      quotation_master_.Subsidy_Amount,
+      quotation_master_.NetTotal,
+      quotation_master_.Product_Name,
+      quotation_master_.Warranty,
+      quotation_master_.Terms_And_Conditions,
+      quotation_master_.Quotation_Status_Id,
+      quotation_master_.Quotation_Status_Name,
+      quotation_master_.Created_By,
+      quotation_master_.Description,
+      JSON.stringify(quotation_master_.items),
+      JSON.stringify(quotation_master_.bill_of_materials),
+      JSON.stringify(quotation_master_.production_chart),
+      quotation_master_.advance_percentage,
+      quotation_master_.onmaterialdelivery_percentage,
+      quotation_master_.onWork_completetion_percentage,
+      quotation_master_.System_Price_Excluding_KSEB_Paperwork,
+      quotation_master_.KSEB_Registration_Fees_KW,
+      quotation_master_.KSEB_Feasibility_Study_Fees,
+      quotation_master_.Additional_Structure_Work,
+      quotation_master_.TaxableAmount,       
+      quotation_master_.TotalGSTAmount,      
+      quotation_master_.TotalGSTPercent,   
+      quotation_master_.TotalAdCESS        
+    ],
+    callback
+  );
+}
 
-  var items_ = JSON.stringify(quotation_master_.items);
-  var bill_of_materials_ = JSON.stringify(quotation_master_.bill_of_materials)
-  var production_chart_ = JSON.stringify(quotation_master_.production_chart);
-  return db.query("CALL Save_quotation_master(" +
-    "@Quotation_Master_Id_ :=?, @Customer_Id_ :=?, @PaymentTerms_ :=?, @Payment_Term_Description_ :=?, " +
-    "@TotalAmount_ :=?, @Subsidy_Amount_ :=?, @NetTotal_ :=?, @Product_Name_ :=?, @Warranty_ :=?, " +
-    "@Terms_And_Conditions_ :=?, @Quotation_Status_Id_ :=?, @Quotation_Status_Name_ :=?, @Created_By_ :=?, " +
-    "@Description_ :=?, @items_ :=?, @bill_of_materials_ :=?, @production_chart_ :=?, " +
-    "@advance_percentage_ :=?, @onmaterialdelivery_percentage_ :=?, @onWork_completetion_percentage_ :=?, " +
-    "@System_Price_Excluding_KSEB_Paperwork_ :=?, @KSEB_Registration_Fees_KW_ :=?, " +
-    "@KSEB_Feasibility_Study_Fees_ :=?, @Additional_Structure_Work_ :=?)",
-[
-    quotation_master_.Quotation_Master_Id,
-    quotation_master_.Customer_Id,
-    quotation_master_.PaymentTerms,
-    quotation_master_.Payment_Term_Description,
-    quotation_master_.TotalAmount,
-    quotation_master_.Subsidy_Amount,
-    quotation_master_.NetTotal,
-    quotation_master_.Product_Name,
-    quotation_master_.Warranty,
-    quotation_master_.Terms_And_Conditions,
-    quotation_master_.Quotation_Status_Id,
-    quotation_master_.Quotation_Status_Name,
-    quotation_master_.Created_By,
-    quotation_master_.Description,
-    JSON.stringify(quotation_master_.items),
-    JSON.stringify(quotation_master_.bill_of_materials),
-    JSON.stringify(quotation_master_.production_chart),
-    quotation_master_.advance_percentage,
-    quotation_master_.onmaterialdelivery_percentage,
-    quotation_master_.onWork_completetion_percentage,
-    quotation_master_.System_Price_Excluding_KSEB_Paperwork,
-    quotation_master_.KSEB_Registration_Fees_KW,
-    quotation_master_.KSEB_Feasibility_Study_Fees,
-    quotation_master_.Additional_Structure_Work
-], callback);
-
- }
  ,
  Delete_quotation_master:function(quotation_master_Id_,callback)
  { 
